d9a55292e4e3d0c01f6d6f4d9b007e946f81bcd7
AliceLavinia <alicelhcv@gmail.com>
2025-05-08 20:10:50 -0300

    Update exercicio1.go

--- exercicio1.go
+++ exercicio1.go
@@ -2,10 +2,20 @@ package main
 
 import "fmt"
 
-func main() {
-	fmt.Println("Hello World")
-}
+// func main() {
+// 	fmt.Println("Hello World")
+// }
 
-func Exercicio1() {
-    
+func Main() {
+    var ano int = 2025
+	var pi float64 = 3.14
+	var nome string = "Alice"
+	var casada bool = false
+
+	fmt.Println("O ano é igual",ano)
+	fmt.Println("O valor de pi é igual",pi)
+	fmt.Println("O seu nome é ",nome)
+	fmt.Println("Seu status de casada é",casada)
+
+
 }
